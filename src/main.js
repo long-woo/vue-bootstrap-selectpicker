f2882fd9fb318963b40d396bb45e742d0c36c100
long.woo <longwunet@outlook.com>
2018-06-16 20:19:50 +0800

    🔥🔥🔥初始化提交🚀

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// Nprogress
+import Nprogress from 'nprogress'
+
+Vue.config.productionTip = false
+
+// use
+Vue.nprogress = Vue.prototype.$nprogress = Nprogress
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
