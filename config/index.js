25a8971565f832c7ca9360ae207379ae763995bd
long.woo <longwunet@outlook.com>
2018-07-05 12:51:27 +0800

    增加`docs`文件夹

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist-web/index.html'),
+    index: path.resolve(__dirname, '../docs/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist-web'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
